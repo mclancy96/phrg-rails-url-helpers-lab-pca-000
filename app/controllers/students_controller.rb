2e5ab090c3dbfeff04807198f15533c8e04e1157
Mike <mclancy96@gmail.com>
2025-08-13 13:48:21 -0400

    Fix routes and controller to use custom path

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: %i[show activate activate_show]
 
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate_show
+  end
+
+  def activate
+    @student.update(active: !@student.active)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: %i[index show]
+
+  get '/students/:id/activate', to: 'students#activate_show', as: 'activate_student'
+  post '/students/:id/activate', to: 'students#activate'
 end
